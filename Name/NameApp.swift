13ba28953285598dc3df54b8a55c72c24e7257bc
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2022-08-08 12:11:15 +0300

    Initial Commit

---
+++ Name/NameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameApp.swift
+//  Name
+//
+//  Created by Maris Lagzdins on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
